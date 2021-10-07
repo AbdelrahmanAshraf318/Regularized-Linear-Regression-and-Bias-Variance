bda3fabdb0e783800d1a27f1aa51d2e01afb95eb
Abdelrahman Ashraf <abdelrahmanashraf25102000@gmail.comm>
2021-10-07 19:23:35 +0200

    linearRegressionCostFunction

--- linearRegCostFunction.m
+++ linearRegCostFunction.m
@@ -19,17 +19,8 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
-
-
-
-
-
-
-
-
-
-
-
+hypothesis = X * theta;
+J = (sum((hypothesis - y) .^ 2) + lambda * sum(theta(2:end) .^ 2)) / (2 * m);
 % =========================================================================
 
 grad = grad(:);
